975d6e990effbf1dfc9b74342163be1c3cd760a1
Curtis Neely <curtisneely65@outlook.com>
2020-11-09 02:19:11 -0500

    Got analytics started, about to install some chart libraries

---
+++ RecipeManger/RecipeManager/RecipeManager/Controllers/AnalyticsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RecipeManager.Data;
+using RecipeManager.Models;
+using RecipeManager.ViewModels;
+
+namespace RecipeManager.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AnalyticsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index(DateTime? StartDate, DateTime? EndDate)
+        {
+            var recipeCount = _context.Recipes.Count();
+            var privateRecipeCount = _context.Recipes.Count(r => r.IsPublic == false);
+            var publicRecipeCount = _context.Recipes.Count(r => r.IsPublic == true);
+            var userCount = _context.Users.Count();
+
+            AnalyticsViewModel analytics = new AnalyticsViewModel()
+            {
+                recipeCount = recipeCount,
+                privateRecipeCount = privateRecipeCount,
+                publicRecipeCount = publicRecipeCount,
+                userCount = userCount
+            };
+
+            return View(analytics);
+        }
+    }
+}
---
+++ RecipeManger/RecipeManager/RecipeManager/ViewModels/AnalyticsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeManager.ViewModels
+{
+    public class AnalyticsViewModel
+    {
+        public int recipeCount { get; set; }
+        public int privateRecipeCount { get; set; }
+        public int publicRecipeCount { get; set; }
+        public int userCount { get; set; }
+    }
+}
