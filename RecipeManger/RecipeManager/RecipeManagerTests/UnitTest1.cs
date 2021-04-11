b5c5abc0e5564a2f71e5d80fdc7f8cdb8033e1ce
Curtis Neely <curtisneely65@outlook.com>
2021-04-11 15:31:09 -0600

    Added a RecipeManagerTests project

---
+++ RecipeManger/RecipeManager/RecipeManagerTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RecipeManagerTests
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
